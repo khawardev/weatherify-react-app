aed1215e59a8bb5336806c6ec086a43eddbc3d7e
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-27 21:29:06 +0500

    update

--- src/components/forcastCurrent/ForcastCurrent.jsx
+++ src/components/forcastCurrent/ForcastCurrent.jsx
@@ -2,6 +2,7 @@
 import Current from '../Current/Current'
 import Forcast from '../forcast/Forcast';
 const ForcastCurrent = ({data,ForcastData,loading}) => {
+  const days = ForcastData?.length;
   return (
     <>
     <div className=' w-11/12 mx-auto md:flex   gap-5 '>
@@ -10,7 +11,7 @@ const ForcastCurrent = ({data,ForcastData,loading}) => {
           </div>
           <div className='md:w-1/2'>
             
-            <Forcast  text={`${ForcastData?.length} Days Forcast`} grid={'xl:grid-cols-4 sm:grid-cols-3 grid-cols-2'} ForcastData={ForcastData} />
+            <Forcast  text={`${days === undefined ? '' : days} Days Forcast`} grid={'xl:grid-cols-4 sm:grid-cols-3 grid-cols-2'} ForcastData={ForcastData} />
           </div>
         </div>
     
